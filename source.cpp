bbafc5d266f370676a13584fd20dee5be25c0a02
Kevin Corpeny <kaimed00@gmail.com>
2017-10-30 15:52:31 -0700

    VC++ working version
    The decode seems to work in this version -- haven't tested extensively yet.

---
+++ source.cpp
@@ -0,0 +1,11 @@
+#include "morse_coder.h"
+
+using namespace std;
+
+int main(){
+  morse_coder myCoder;
+  const string in = ".._. .._ _._. _._";
+  cout<<myCoder.decode(in);
+  cin.get();
+  return 0;
+}
